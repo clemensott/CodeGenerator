a78d313b7cfd2a61584c875dd1f88f636d6b3287
ShelbyBoss <42971547+ShelbyBoss@users.noreply.github.com>
2018-11-05 17:19:20 +0100

    Uses Same ControlLtype

--- DependencyProperty/DependencyPropertyControl.xaml.cs
+++ DependencyProperty/DependencyPropertyControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace CodeGenerator.DependencyProperty
@@ -15,12 +16,24 @@ namespace CodeGenerator.DependencyProperty
 
         private void BtnGenerate_Click(object sender, RoutedEventArgs e)
         {
+            SetSameControlType();
+
             CodeCopyService.Current.CopyWholeCode(DataContext as CodeDependencyPropertiesService);
         }
 
         private void BtnNext_Click(object sender, RoutedEventArgs e)
         {
+            SetSameControlType();
+
             CodeCopyService.Current.CopyNextCodePart(DataContext as CodeDependencyPropertiesService);
+        }
+
+        private void SetSameControlType()
+        {
+            CodeDependencyPropertiesService service = DataContext as CodeDependencyPropertiesService;
+            string controlType = service.CodeObjects.Select(o => o.ControlType).FirstOrDefault(t => !string.IsNullOrWhiteSpace(t));
+
+            foreach (DependencyProperty property in service.CodeObjects) property.ControlType = controlType;
         }
 
         private void BtnStop_Click(object sender, RoutedEventArgs e)
